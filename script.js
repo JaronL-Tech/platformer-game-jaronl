b0d52c65ea8d7163e79b32faa45efd6afa95e6fa
Jaron Leach <jaronleach98@gmail.com>
2024-08-20 23:50:22 -0400

    created a new player option and a startgame function following that I created a startBtn eventlisterner that was on click to allow players to press the startGame button.

--- script.js
+++ script.js
@@ -55,3 +55,12 @@ class Player {
     }
   }
 }
+const player = new Player();
+
+const startGame = () => {
+  canvas.style.display = "block";
+  startScreen.style.display = "none";
+  player.draw();
+};
+
+startBtn.addEventListener("click", startGame);
