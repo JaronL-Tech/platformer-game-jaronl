45b034ad238333653411d0730a1087ec41741e4e
Jaron Leach <jaronleach98@gmail.com>
2024-08-22 04:12:02 -0400

    updated the animate functions to add foreach loops for the checkpoints function to call them.

--- script.js
+++ script.js
@@ -133,6 +133,10 @@ const animate = () => {
     platform.draw();
   });
 
+  checkpoints.forEach((checkpoint) => {
+    checkpoint.draw();
+  });
+
   player.update();
 
   if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
@@ -149,9 +153,17 @@ const animate = () => {
       platforms.forEach((platform) => {
         platform.position.x -= 5;
       });
+
+      checkpoints.forEach((checkpoint) => {
+        checkpoint.position.x -= 5;
+      });
     } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
       platforms.forEach((platform) => {
         platform.position.x += 5;
+      });
+
+      checkpoints.forEach((checkpoint) => {
+        checkpoint.position.x += 5;
       });
     }
   }
